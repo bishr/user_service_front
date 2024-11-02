22ac8e2b899961ca0a03579fa79b20e4bbb867f6
bishr <bishr.zaghloul@gmail.com>
2024-11-02 21:59:22 +0300

    Initial commit

---
+++ src/app/auth/auth.state.ts
@@ -0,0 +1,6 @@
+import { AuthState } from './auth.model';
+
+export const initialAuthState: AuthState = {
+  isAuthenticated: JSON.parse(localStorage.getItem('isAuthenticated') || 'false'),
+  token: null,
+};
